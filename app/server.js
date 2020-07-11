d0f5e40fe70d3439c10bf2f7f9825f83f0f5abca
Kaleween <kaleween@gmail.com>
2020-07-11 13:33:15 +0100

    Final test before real demo

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur notre démonstration du mspr-i1-tpre716-2020\n');
+  res.send('Bonjour le jury\n');
 });
 
 app.listen(PORT, HOST);
